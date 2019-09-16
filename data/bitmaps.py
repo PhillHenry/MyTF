6feaf4daa8bd8bec750143def71cb1d4169a8067
PhillHenry <phillhenry@gmail.com>
2019-09-16 05:57:55 +0100

    Failing test - fix me.

--- data/bitmaps.py
+++ data/bitmaps.py
@@ -9,10 +9,16 @@ class RandomBitmap:
         self.m = m
 
     def add_random_points(self, npts):
+        if npts > (self.n * self.m):
+            raise Exception('n = {}, m = {}, npts = {}'.format(self.n, self.m, npts))
         xs = np.random.randint(0, self.m, npts)
         ys = np.random.randint(0, self.n, npts)
-        coords = np.vstack([xs,ys]).T
+        coords = np.vstack([xs,ys]).T # https://stackoverflow.com/questions/26193386/numpy-zip-function
+        pts = set()
         for (x, y) in coords:
             self.matrix[x][y] = 1
+            pts.add((x, y))
+        if len(pts) < npts:
+            self.add_random_points(npts - len(pts))
         return self.matrix
 
--- tests/data/test_random_bitmap.py
+++ tests/data/test_random_bitmap.py
@@ -1,6 +1,12 @@
 from data import bitmaps
-
+import numpy as np
 
 def test_bitmap_contains_expected_num_of_points():
-    bitmap = bitmaps.RandomBitmap(9, 99)
-    bitmap.add_random_points(1000)
+    n_pts = 1000
+    bitmap = bitmaps.RandomBitmap(42, 99)
+    matrix = bitmap.add_random_points(n_pts)
+    total = 0
+    for row in matrix:
+        total += len(list(filter(lambda x: x == 1, row)))
+    assert len == total
+
