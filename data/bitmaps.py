70fe0de39788f860d15130ff69aaefa16f3745ea
PhillHenry <phillhenry@gmail.com>
2019-09-18 05:11:52 +0100

    Bug fix.

--- data/bitmaps.py
+++ data/bitmaps.py
@@ -11,15 +11,19 @@ class RandomBitmap:
     def add_random_points(self, npts):
         if npts > (self.n * self.m):
             raise Exception('n = {}, m = {}, npts = {}'.format(self.n, self.m, npts))
+        self._add_points(npts)
+        total = self.num_points()
+        while total < npts:
+            self.add_random_points(npts - total)
+            total = self.num_points()
+        return self.matrix
+
+    def _add_points(self, npts):
         xs = np.random.randint(0, self.m, npts)
         ys = np.random.randint(0, self.n, npts)
-        coords = np.vstack([xs,ys]).T  # https://stackoverflow.com/questions/26193386/numpy-zip-function
+        coords = np.vstack([xs, ys]).T  # https://stackoverflow.com/questions/26193386/numpy-zip-function
         for (x, y) in coords:
             self.matrix[x][y] = 1
-        total = self.num_points()
-        if total < npts:
-            self.add_random_points(npts - total)
-        return self.matrix
 
     def num_points(self):
         total = 0
--- tests/data/test_random_bitmap.py
+++ tests/data/test_random_bitmap.py
@@ -4,7 +4,7 @@ from data import bitmaps
 def test_bitmap_contains_expected_num_of_points():
     i = 42
     j = 99
-    npts = int((i * j) / 10)
+    npts = int((i * j) / 5)
     bitmap = bitmaps.RandomBitmap(i, j)
     matrix = bitmap.add_random_points(npts)
     assert bitmap.num_points() == npts
