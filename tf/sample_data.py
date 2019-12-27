50f37cf70eee18203904c7ba58537f4313a974eb
PhillHenry <phillhenry@gmail.com>
2019-12-27 10:37:25 +0000

    Looks better.

--- tf/sample_data.py
+++ tf/sample_data.py
@@ -24,17 +24,28 @@ def do_plot(fig, offset, n_rows, matrics):
     nOfEach = len(matrics)
     for i in range(nOfEach):
         ax = fig.add_subplot(n_rows, nOfEach, i + 1 + offset)
-        ax.set_yticklabels([])  # https://stackoverflow.com/questions/37039685/hide-axis-values-in-matplotlib
-        ax.set_xticklabels([])
+        do_axes(ax)
         plt.imshow(matrics[i])
+
+
+def do_axes(ax):
+    ax.set_yticklabels([])  # https://stackoverflow.com/questions/37039685/hide-axis-values-in-matplotlib
+    ax.set_xticklabels([])
+    ax.yaxis.set_ticks_position('none')
+    ax.xaxis.set_ticks_position('none')
+    ax.set_axis_off()
 
 
 if __name__ == '__main__':
     samples = Sample(40, 100, 100)
-    fig = plt.figure(0)
-
     nOfEach = 3
     n_rows = 2
+    fig, (ax1, ax2) = plt.subplots(n_rows, 1)
+    plt.title("Simulated network connections (machines vs ports)")
+    ax1.set_title('Port scanned', pad=-50.0)
+    do_axes(ax1)
+    ax2.set_title('Normal activity', pad=-50.0)
+    do_axes(ax2)
 
     positive_matrices = Sample.matrices_of(samples.training_data, 1)
     do_plot(fig, 0, n_rows, positive_matrices[0:nOfEach])
@@ -42,5 +53,4 @@ if __name__ == '__main__':
     negative_matrices = Sample.matrices_of(samples.training_data, 0)
     do_plot(fig, nOfEach, n_rows, negative_matrices[0:nOfEach])
 
-    # plt.title("Simulated network connections (machines vs ports)")
     plt.show()
