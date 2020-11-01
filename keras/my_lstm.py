8ae354b2ec9c53274648f5549a8e2ca85c2fcefd
Phillip Henry <PhillHenry@gmail.com>
2020-11-01 10:17:26 +0000

    Refactoring.

--- keras/my_lstm.py
+++ keras/my_lstm.py
@@ -21,16 +21,24 @@ def reshape(xs):
 
 # define input sequence
 n_in = 50
-cycles_x3 = raw_data(3, n_in)
 # reshape input into [samples, timesteps, features]
-cycles_x3 = reshape(cycles_x3)
+n_samples = 2
+
+cycles_x3 = raw_data(3, n_in)
+for i in range (n_samples - 1):
+    data = raw_data((i + 1), n_in)
+    cycles_x3 = np.concatenate((cycles_x3, data), axis=None)
+cycles_x3 = cycles_x3.reshape(n_samples, n_in, 1)
+
+
+#cycles_x3 = np.concatenate((raw_data(3, n_in), raw_data(2, n_in)), axis=None).reshape(n_samples, n_in, 1)
 
 cycles_x1 = raw_data(1, n_in)
 cycles_x1 = reshape(cycles_x1)
 
 # define model
 model = Sequential()
-model.add(LSTM(100, activation='relu', input_shape=(n_in,1)))
+model.add(LSTM(100, activation='relu', input_shape=(n_in, 1)))
 model.add(RepeatVector(n_in))
 model.add(LSTM(100, activation='relu', return_sequences=True))
 model.add(TimeDistributed(Dense(1)))
@@ -40,10 +48,9 @@ model.fit(cycles_x3, cycles_x3, epochs=300, verbose=0)
 plot_model(model, show_shapes=True, to_file='reconstruct_lstm_autoencoder.png')
 # demonstrate recreation
 yhat = model.predict(cycles_x1, verbose=0)
-predictions = yhat[0,:,0]
+predictions = yhat[0, :, 0]
 print(predictions)
 
-plt.plot(raw_data(3, n_in))
 plt.plot(raw_data(1, n_in))
 plt.plot(predictions)
 plt.show()
